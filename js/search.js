9c2770fb39925ef3a1ffd56dd002e7d47d794ad9
FIANERD <mattiasson.sofia@gmail.com>
2021-03-01 22:44:04 +0100

    Changed text td to center

--- js/search.js
+++ js/search.js
@@ -1,43 +1,31 @@
 "use strict";
 const searchInput = document.getElementById("searchInput");
-const searchBox = document.querySelector(".search-box");
-let products = JSON.parse(localStorage.getItem("Products")) ?? [];
 
-// let user = products[1];
+let products = JSON.parse(localStorage.getItem("Products")) ?? [];
 
 searchInput.addEventListener("keyup", (event) => {
   event.preventDefault();
-  let searchProducts = event.target.value;
+  let searchProducts = event.target.value.toLowerCase();
   if (searchProducts.length) {
     products = products.filter((product) => {
       return product.Name.toLowerCase().startsWith(
         searchProducts.toLowerCase()
       );
     });
-    // products = products.map((searchProducts) => {
-    //   return (searchProducts = "<td>" + searchProducts + "</td>");
-    // });
     console.log(products);
-    // } else {
-    // }
-    // showProductSearch(searchProducts);
-
-    // function showProductSearch(list) {
-    //   let listProducts;
-    //   if (!list.length) {
-    //   } else {
-    //     listProducts = list.join("product");
-    //   }
-    //   productTable.textContent = listProducts;
-    // }
-
-    const productTable = document.querySelector("tbody");
-
-    delete productTable.querySelector("tr");
-
-    products.forEach((product) => {
-      let td = productTable.querySelector("td");
-      // td[1].textContent = product.Name;
-    });
   }
+  const productTable = document.querySelector("tbody");
+
+  delete productTable.querySelector("tr");
+
+  // products.forEach((product) => {
+  //   if (product !== "") {
+  //   }
+  // });
+
+  // products.forEach((product) => {
+  //   let name = product.Name;
+  //   console.log(name);
+  // });
+  // console.log(products);
 });
