ca71e3ebc0800fc496a4fdac0be050f474ccda16
FIANERD <mattiasson.sofia@gmail.com>
2021-03-03 14:25:16 +0100

    Its printing right.. just have to clear body

--- js/search.js
+++ js/search.js
@@ -1,29 +1,38 @@
 "use strict";
+
+const productTable = document.getElementById("product-table");
+
 const searchInput = document.getElementById("searchInput");
 
-const productTable = document.querySelectorAll("tbody tr");
+const productTBody = document.querySelectorAll("tbody");
 
 const productTemplate = document.querySelector("#product-row-template");
 
-// delete productTable.querySelector("tr");
+delete productTable.querySelector("tr");
 
 let products = JSON.parse(localStorage.getItem("Products")) ?? [];
 
 searchInput.addEventListener("keyup", (event) => {
   event.preventDefault();
-  const searchProducts = event.target.value.toLowerCase();
-  productTemplate.textContent = "";
+
+  const searchProducts = event.target.value.toUpperCase();
+
+  // productTBody.textContent = "";
+
   const userSearchProducts = products.filter((product) => {
-    return product.Name.toLowerCase().startsWith(searchProducts);
+    return product.Name.toUpperCase().startsWith(searchProducts);
   });
-  console.log("Products", userSearchProducts);
 
   userSearchProducts.forEach((search) => {
-    const tdProduct = document.createElement("td");
-    tdProduct.textContent = search.Name;
-    productTemplate.appendChild(tdProduct);
+    let clone = template.content.cloneNode(true);
+
+    let td = clone.querySelectorAll("td");
+    td[0].textContent = search.ArtNumber.toUpperCase();
+    td[1].textContent = search.Name.toUpperCase();
+    td[2].textContent = search.Price;
+
+    tbody.appendChild(clone);
   });
-  if (searchProducts === "") {
-    productTemplate.textContent = "";
-  }
+
+  console.log("Products", userSearchProducts);
 });
